b69a9029d3a23c9a7bb3fa4018f791b9e97e260d
ahk <ahk@192.168.1.6>
2025-07-31 18:44:10 +0300

    index.html

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+
 
 // Veritabanı bağlantısı
 const pool = mysql.createPool({
