1ab012ff74537f87f65ae4c20dc22d888f926951
ahk <ahk@192.168.1.6>
2025-07-31 16:43:42 +0300

    bug fix

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
-
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
